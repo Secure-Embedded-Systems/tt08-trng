011ced1752328c67c375dbd174436ede35331873
Roy1707018 <aroy1@wpi.edu>
2024-08-29 19:32:32 -0400

    design

--- test/test.py
+++ test/test.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
@@ -11,7 +8,7 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="ns")
+    clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -27,16 +24,14 @@ async def test_project(dut):
 
     # Set the input values you want to test
     dut.ui_in.value = 3
-    await ClockCycles(dut.clk, 2)
-    output_value = dut.uo_out.value
-    dut._log.info(f"Output value: {output_value}")
+    #dut.uio_in.value = 30
 
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 2)
     dut.ui_in.value = 0
     # Read and log the output value
-    await ClockCycles(dut.clk, 2)
     output_value = dut.uo_out.value
     dut._log.info(f"Output value: {output_value}")
-
     await ClockCycles(dut.clk, 2)
     dut.ui_in.value = 11
     await ClockCycles(dut.clk, 1)
@@ -44,6 +39,3 @@ async def test_project(dut):
     # Read and log the output value
     output_value = dut.uo_out.value
     dut._log.info(f"Output value: {output_value}")
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
