f415fc2fe4779a462ac55dd1906b9ad0f4fcf994
Arna Roy <arnaroy@Arnas-Laptop.local>
2024-08-29 10:26:38 -0400

    RO

--- src/ring_osc.v
+++ src/ring_osc.v
@@ -39,10 +39,18 @@ module ring_osc(input wire  rst_n,
         ro_out <= 1'b0;
     end */
    (* keep = "true" *) wire cq4;
+   // A temporary signal to initialize q4 at the start
+    reg q4_init;
+
+    // Use the initialized signal or feedback from q3
+    assign q4 = q4_init ? 1'b1 : q4;
+
+    initial begin
+        q4_init = 1'b1;  // Set q4_init to 1 at the beginning
+        #1;              // Wait 1 time unit
+        q4_init = 1'b0;  // Disable the initialization after 1 time unit
+    end
    assign cq4 = (en & q4);
-   initial begin
-    q4 = 1'b1;  // Initialize q4 to 1
-   end
    (* keep = 1 *) cinv cinv1(.a(cq4),.q(q0));
    (* keep = 1 *) cinv cinv2(.a(q0), .q(q1));
    (* keep = 1 *) cinv cinv3(.a(q1), .q(q2));
