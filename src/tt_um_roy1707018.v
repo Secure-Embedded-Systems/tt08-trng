e3f6a7555c421fab0ac9e85f88f4b01da7fab345
DillibabuShanmugam <90065623+DillibabuShanmugam@users.noreply.github.com>
2024-08-30 14:35:28 -0400

    Update tt_um_roy1707018.v

--- src/tt_um_roy1707018.v
+++ src/tt_um_roy1707018.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_roy1707018_roy1707018 (
+module tt_um_roy1707018 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
